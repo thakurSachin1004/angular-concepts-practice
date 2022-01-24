46d1f0b7814ac7d9cd270509c160338649b2f794
sachin-personal <sachin-personal@Sachins-MacBook-Pro.local>
2022-01-24 21:24:52 +0530

    learned about HostBinding and HostListener

--- directives-start/src/app/directives/better-highlight/better-highlight.directive.ts
+++ directives-start/src/app/directives/better-highlight/better-highlight.directive.ts
@@ -1,4 +1,11 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from "@angular/core";
+import {
+  Directive,
+  ElementRef,
+  HostBinding,
+  HostListener,
+  OnInit,
+  Renderer2,
+} from "@angular/core";
 
 @Directive({
   selector: "[appBetterHighlight]",
@@ -6,13 +13,37 @@ import { Directive, ElementRef, OnInit, Renderer2 } from "@angular/core";
 export class BetterHighlightDirective implements OnInit {
   constructor(private elref: ElementRef, private renderer: Renderer2) {} // Angular will pass the element ref on which we have used this directive.
 
+  // instead of Renderer using Host Binding to change properties of HTML elt. Here we need style property and to change background So we used style.background as property.
+  @HostBinding("style.background") backgroundColor: string = "transparent";
+
   ngOnInit(): void {
     this.renderer.setStyle(
       this.elref.nativeElement,
       "background-color",
-      "blue"
+      "transparent"
     );
-    this.renderer.setStyle(this.elref.nativeElement, "color", "white");
     this.renderer.setStyle(this.elref.nativeElement, "padding", "20px");
+  }
+
+  @HostListener("mouseenter") onHover(eventData: Event) {
+    // using Renderer
+
+    // this.renderer.setStyle(
+    //   this.elref.nativeElement,
+    //   "background-color",
+    //   "green"
+    // );
+    this.backgroundColor = "green";
+  }
+
+  @HostListener("mouseleave") onMouseLeave(eventData: Event) {
+    // using Renderer
+
+    // this.renderer.setStyle(
+    //   this.elref.nativeElement,
+    //   "background-color",
+    //   "blue"
+    // );
+    this.backgroundColor = "transparent";
   }
 }

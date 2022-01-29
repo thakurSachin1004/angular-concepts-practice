52511cb69dd2dfb7fe56e66e5a39bce3e95c6132
thakursachin1004 <sachinst000@gmail.com>
2022-01-29 21:50:31 +0530

    fetching routing params using ActivatedRouter and params Observable

--- routing-start/src/app/home/home.component.ts
+++ routing-start/src/app/home/home.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router, ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit {
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  onClick() {
+    this.router.navigate(["/servers"]);
   }
-
 }
--- routing-start/src/app/users/user/user.component.ts
+++ routing-start/src/app/users/user/user.component.ts
@@ -1,16 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Params } from "@angular/router";
 
 @Component({
-  selector: 'app-user',
-  templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  selector: "app-user",
+  templateUrl: "./user.component.html",
+  styleUrls: ["./user.component.css"],
 })
 export class UserComponent implements OnInit {
-  user: {id: number, name: string};
+  user: { id: number; name: string };
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
-  }
+    this.user = {
+      name: this.route.snapshot.params["name"],
+      id: this.route.snapshot.params["id"],
+    };
 
+    // params is a observable to which we have subscribed.
+    this.route.params.subscribe((params: Params) => {
+      this.user = {
+        name: params["name"],
+        id: params["id"],
+      };
+    });
+  }
 }
